7d7922b2fe1c81537f24f2aed61ed246560e2f39
Jiiroo <jhonheron.carsocho@gmail.com>
2021-12-18 12:09:49 +0800

    add schema if not exist

--- libs/baseclass/scanner.py
+++ libs/baseclass/scanner.py
@@ -77,16 +77,25 @@ class Scanner(Screen):
         self.ids.cam_respo.texture = texture
 
     def data_base(self, data):
-        conn = mysql.connector.connect(
-                    host = '127.0.0.1',
-                    user = 'root',
-                    passwd = '',
-                    database = 'trial'
-                    )
-        
-        cur = conn.cursor()
-        
-        # creates table and columns inside database
+        try:
+            conn = mysql.connector.connect(
+                            host = '127.0.0.1',
+                            user = 'root',
+                            passwd = '1234',
+                            database="sql_project"
+                            )
+       
+        except (mysql.connector.errors.ProgrammingError):
+            conn = mysql.connector.connect(
+                        host = '127.0.0.1',
+                        user = 'root',
+                        passwd = '1234'
+                        )
+            cur = conn.cursor()
+            cur.execute("CREATE DATABASE sql_project")   
+
+        cur = conn.cursor()   
+
         cur.execute("""CREATE TABLE IF NOT EXISTS customers (
                             id MEDIUMINT NOT NULL AUTO_INCREMENT,
                             PRIMARY KEY (id),
